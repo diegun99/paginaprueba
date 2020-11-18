6aa08ec841fb17f0b51647a7e028ae8523bd84fc
diegun99 <diegun123456789@gmail.com>
2020-11-17 23:48:22 -0500

    arreglos llave valor

--- pagina2.php
+++ pagina2.php
@@ -15,25 +15,105 @@
 
     <?php
 
-    $persona = array(
-        'nombre' => 'Jose',
-        'apellido' => 'Jimenez',
-        'telefono' => '3112452130',
-        'direccion' => 'carrera 9 # 3-12'
-    );
+        $persona = array(
+            'nombre' => 'Jose',
+            'apellido' => 'Jimenez',
+            'telefono' => '3112452130',
+            'direccion' => 'carrera 9 # 3-12'
+        );
 
-    ?>
-    <pre>
+        ?>
+        <pre>
+            <?php
+        echo print_r($persona);
+        echo var_dump($persona);
+        echo $persona['apellido'];
+
+        $persona['apellido'] = 'martinez';
+        echo $persona['apellido'];
+
+        $deportes = array("juan",'caro', 'miguel');
+
+        ?>
+
+        </pre>
+
+        <ul>
         <?php
-    echo print_r($persona);
+
+    foreach($deportes as $deporte):
+        
+        ?>
+        
+        
+       <li> <?php echo $deporte; ?>  </li>
+
+      <?php  
+
+    endforeach;
 
     ?>
 
-    </pre>
+    </ul>
+
 
     <?php
 
+    echo "<ul>";
+
+    foreach($persona as $key => $valor) :
+
+        echo "<li>".$key. ": ". $valor. "</li>";
+
+    endforeach;
+
+    
+
+    echo "<ul>";
+
+    $i=0;
+
+    while($i<=10){
+        echo $i;
+        echo "<br>";
+        $i++;
+
+    }
+
     ?>
+
+    <h2>recorriendo arreglo con while</h2>
+
+    <?php
+
+    $i=0;
+
+    ?>
+
+    <ul>
+
+    <?php
+
+    while ($i < count($deportes)) {
+
+        ?>
+
+       <li> <?php echo $deportes[$i];  ?> </li>
+
+       <?php
+        $i++;
+    }
+
+    ?>
+
+    </ul>
+
+    <?php
+    echo $i;
+
+    ?>
+
+
 
 
     </div>
